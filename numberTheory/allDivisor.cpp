bf6c5377a98784a0c16c7585759693249f03c320
gulshanyadav01 <gulshany01@gmail.com>
2020-09-25 19:47:44 +0530

    only number theory

---
+++ numberTheory/allDivisor.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void printAllDivisor(int n ) {
+    for(int i= 1; i*i <= n; i++) {
+
+        if(n% i == 0 ){
+            cout<< i <<" ";
+            if(i!= n/i){
+                cout<< n/i << " ";
+            }
+        }
+    
+    }
+}
+int main(){
+    int n; 
+    cin>> n;
+    printAllDivisor( n );
+
+}
