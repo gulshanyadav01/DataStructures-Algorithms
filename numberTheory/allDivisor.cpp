870c99a8e41ba3b12c932f52c0e4a75f4e86d08b
gulshanyadav01 <gulshany01@gmail.com>
2020-09-25 20:03:47 +0530

    yet to come

--- numberTheory/allDivisor.cpp
+++ numberTheory/allDivisor.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 using namespace std;
+void print(){
+    cout<<"hello"<<endl;
+}
 
 void printAllDivisor(int n ) {
+    // theta(sqrt(n)) time complexity
     for(int i= 1; i*i <= n; i++) {
 
         if(n% i == 0 ){
