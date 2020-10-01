08103de5cffe90763496df60212558aa0b921d4b
gulshanyadav01 <gulshany01@gmail.com>
2020-10-01 20:33:00 +0530

    yet to come

---
+++ Array/leaderInArray.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+// this has time complexity Q(N^2);
+void leaderInArray (int arr[], int num){
+    for(int i = 0; i < num; i++){
+        bool flag = false;
+        for(int j = i + 1; j < num; j++){
+            if(arr[i] <= arr[j]){
+                flag = true;
+                break;
+            }
+        }
+        if(flag == false){
+            cout << arr[i] << " ";
+        }
+    }
+
+}
+// this have Q(n) time complexity;
+void leaderInArray2(int arr[], int num ){
+    int currLead = arr[num-1];
+    cout << currLead << " ";
+    for(int i = num -2; i >= 0; i--){
+        if(currLead < arr[i]) {
+            currLead = arr[i]; 
+            
+            cout << currLead <<" ";
+        }
+        
+}
+}
+int main(){
+    int num;
+    cin >> num;
+    int arr[num ];
+    for(int i= 0 ;i<num ;i++){
+        cin >> arr[i];
+    }
+    // leaderInArray(arr, num );
+    leaderInArray2(arr, num);
+
+}
