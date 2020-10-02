78432b3eeb4db6e55b516eb9eaedf0c11b195f57
gulshanyadav01 <gulshany01@gmail.com>
2020-10-02 19:26:31 +0530

    yet to come

---
+++ Array/maxSumArray.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int maxSumArray(int arr[], int num ){
+    int res = arr[0];
+
+    for(int i = 1; i < num; i++){
+        int curr = 0;
+        for(int j = i; j < num; j++){
+            curr += arr[j];
+            res = max(curr, res);
+        }
+    }
+    return res;
+
+}
+int main(){
+    int num;
+    cin >> num;
+    int arr[ num ];
+    for(int i = 0; i < num; i++){
+        cin >> arr[i];
+    }
+    cout << maxSumArray(arr, num );
+    return 0;
+}
