ebadff3e2fdb533648990a41b82882c236829d73
gulshanyadav01 <gulshany01@gmail.com>
2020-10-03 20:44:18 +0530

    yet to come

--- numberTheory/isPrime.cpp
+++ numberTheory/isPrime.cpp
@@ -9,7 +9,8 @@ bool isPrime(int num){
     }
     return true;
 }
-// efficient solutin 
+// efficient solutin Q(sqrt(n))
+
 bool isPrime2(int num){
     if(num ==1 ){
         return false;
@@ -21,10 +22,23 @@ bool isPrime2(int num){
     }
     return true;
 } 
+// mor e efficient solution 
+bool isPrime3(int num){
+    if(num == 1 ){
+        return false;
+    }
+    if(num == 2 || num == 3 ) return true;
+    if(num % 2 == 0 || num % 3 == 0) return false;
+    for(int i = 5; i*i <= num; i++){
+        if(num % i == 0 || num %(i+2) == 0) return false;
+    }
+    return true;
+}
 int main(){
     int num;
     cin >> num;
     // cout << isPrime(num);
-    cout << isPrime2(num);
+    // cout << isPrime2(num);
+    cout << isPrime3(num);
     return 0;
 }
