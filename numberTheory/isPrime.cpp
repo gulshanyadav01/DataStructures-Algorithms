1c53faa78dbd6c0ae64b61a2f4621b5b529be761
gulshanyadav01 <gulshany01@gmail.com>
2020-10-03 20:33:09 +0530

    yet to come

--- numberTheory/isPrime.cpp
+++ numberTheory/isPrime.cpp
@@ -9,9 +9,22 @@ bool isPrime(int num){
     }
     return true;
 }
+// efficient solutin 
+bool isPrime2(int num){
+    if(num ==1 ){
+        return false;
+    }
+    for(int i = 2; i*i < num; i++){
+        if(num %i == 0){
+            return false;
+        }
+    }
+    return true;
+} 
 int main(){
     int num;
     cin >> num;
-    cout << isPrime(num);
+    // cout << isPrime(num);
+    cout << isPrime2(num);
     return 0;
 }
