5811b0f90cac3684760b8336afc1b93e72629ace
gulshanyadav01 <gulshany01@gmail.com>
2020-10-03 16:16:58 +0530

    yet to come

--- numberTheory/gcdOfNumber.cpp
+++ numberTheory/gcdOfNumber.cpp
@@ -25,11 +25,22 @@ int gcdOfTwoNumberEuc(int num1 , int num2 ){
     return num1; 
 
 }
+// gcd of 
+int gcdOfTwoNumber(int num1 , int num2){
+
+    if(num2  == 0){
+        return num1;
+    }
+    return gcdOfTwoNumber(num2, num1%num2);
+
+
+}
 int main(){
     int num1 , num2;
     cin >> num1 >> num2;
     // cout << gcdOfTwoNumberNaive(num1 , num2 );
-    cout << gcdOfTwoNumberEuc(num1 , num2 );
+    // cout << gcdOfTwoNumberEuc(num1 , num2 );
+    cout << gcdOfTwoNumber(num1 , num2);
     return 0;
 
 }
