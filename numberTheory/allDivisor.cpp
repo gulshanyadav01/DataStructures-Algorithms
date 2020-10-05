508c6ab1c377688198485ec85bc11c90aa726098
gulshanyadav01 <gulshany01@gmail.com>
2020-10-05 16:03:28 +0530

    yet to come

--- numberTheory/allDivisor.cpp
+++ numberTheory/allDivisor.cpp
@@ -3,7 +3,25 @@ using namespace std;
 void print(){
     cout<<"hello"<<endl;
 }
+void printAllDivisor2(int num){
+    for(int i = 1; i*i<= num; i++){
+        if(num% i ==0){
+             cout << i << " ";
+             if(i != num%i){
+                 cout << num/i << " ";
 
+             }
+        }
+
+    }
+}
+
+
+
+
+
+
+// this is efficient solution 
 void printAllDivisor(int n ) {
     // theta(sqrt(n)) time complexity
     for(int i= 1; i*i <= n; i++) {
@@ -20,6 +38,7 @@ void printAllDivisor(int n ) {
 int main(){
     int n; 
     cin>> n;
-    printAllDivisor( n );
+    // printAllDivisor( n );
+    printAllDivisor2(n);
 
 }
