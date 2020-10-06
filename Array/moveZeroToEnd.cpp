c0c68a42e9261c59f65a36ad20de1f5df8916c32
gulshanyadav01 <gulshany01@gmail.com>
2020-10-06 17:43:29 +0530

    yet to come

--- Array/moveZeroToEnd.cpp
+++ Array/moveZeroToEnd.cpp
@@ -1,5 +1,28 @@
 #include<iostream>
 using namespace std;
+
+// naive solution 
+void moveZero(int arr[], int num){
+    for(int i= 0; i< num; i++){
+        if(arr[i] == 0){
+            for(int j= i+1; j< num; j++){
+                if(arr[j] != 0){
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                    break;
+                }
+            }
+        }
+    }
+    for(int i= 0; i< num; i++){
+        cout << arr[i] << " ";
+    }
+}
+
+
+
+
 void movesZeroToEnd(int arr[], int num ){
     int count = 0;
     for(int i = 0; i < num; i++){
@@ -22,7 +45,8 @@ int main(){
     for(int i= 0 ;i<num ;i++){
         cin >> arr[i];
     }
-    movesZeroToEnd(arr, num);
+    // movesZeroToEnd(arr, num);
+    moveZero(arr, num);
 
 
 }
