e0ca31fa6a5b5f2f20a658a3a0aa7b5094af629e
gulshanyadav01 <gulshany01@gmail.com>
2020-10-07 20:06:19 +0530

    yet to come

--- Array/consecutive1.cpp
+++ Array/consecutive1.cpp
@@ -13,6 +13,23 @@ int consective1(int arr[], int num ){
     }
     return res;
 }
+// second time 
+int cons(int arr[], int num){
+    int res= 0;
+    int i = 0;
+    while(i< num){
+        if(arr[i] == 1){
+            int count =0;
+            while(arr[i] ==1){
+                count++;
+                res = max(count, res);
+                i++;
+            }
+        }
+        i++;
+    }
+    return res;
+}
 int main(){
      int num;
     cin >> num;
@@ -20,7 +37,8 @@ int main(){
     for(int i = 0; i < num; i++){
         cin >> arr[i];
     }
-    cout << consective1(arr, num );
+    // cout << consective1(arr, num );
+    cout <<cons(arr, num);
     return 0;
 
 }
