2d47076aa8d40de1b452a9edf8197d6c84440ac7
gulshanyadav01 <gulshany01@gmail.com>
2020-10-08 17:54:23 +0530

    yet to come

---
+++ Array/majorElem.cpp
@@ -0,0 +1,31 @@
+/* 
+author: *gulshan yadav*
+date: 08-oct-2020
+*/
+#include<iostream>
+using namespace std;
+int majorElem(int arr[], int num){
+    for(int i= 0; i< num; i++){
+        int count = 0;
+        for(int j= 0; j< num; j++){
+            if(arr[i] == arr[j]){
+                count++;
+            }
+            if(count > num/2){
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+int main(){
+     int num;
+    cin >> num;
+    int arr[num ];
+    for(int i= 0 ;i<num ;i++){
+        cin >> arr[i];
+    }
+    cout << majorElem(arr, num);
+    return 0;
+}
