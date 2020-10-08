676798ae55e62c519c108b424715431d052068e4
gulshanyadav01 <gulshany01@gmail.com>
2020-10-08 16:04:58 +0530

    yet to come

--- Array/maxLenOddEven.cpp
+++ Array/maxLenOddEven.cpp
@@ -1,5 +1,26 @@
 #include<iostream>
 using namespace std;
+// naive solution time Q(N^2)
+
+int maxLenNaive(int arr[],int num){
+    int res = 1;
+    for(int i= 0; i< num; i++){
+        int count = 1; 
+        for(int j= i+1; j< num; j++){
+            if((arr[j]% 2 == 0 ) && (arr[j-1]%2 != 0 ) ||(arr[j]% 2 != 0) && (arr[j-1]%2 == 0)){
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        res = max(count,res);
+    }
+    return res;
+}
+
+
+
 int maxLen(int arr[], int num){
     // int count = 0;
     int res = 1;
@@ -26,7 +47,8 @@ int main(){
     for(int i= 0 ;i<num ;i++){
         cin >> arr[i];
     }
-    cout << maxLen(arr, num);
+    // cout << maxLen(arr, num);
+    cout << maxLenNaive(arr, num);
     return 0;
 
 }
