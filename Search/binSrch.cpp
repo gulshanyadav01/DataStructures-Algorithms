6776c13398df1296bb168fc91ead9355adaebfac
gulshanyadav01 <gulshany01@gmail.com>
2020-10-12 06:12:49 +0530

    yet to com

--- Search/binSrch.cpp
+++ Search/binSrch.cpp
@@ -4,7 +4,7 @@ date: 11-oct-2020
 */
 #include<iostream>
 using namespace std;
-
+// this is iterative solution 
 int binSrchIter(int arr[], int num, int x){
     int low =0; 
     int high = num-1;
@@ -24,6 +24,24 @@ int binSrchIter(int arr[], int num, int x){
     return -1;
 
 }
+// this is recursive solution 
+int binSrchRec(int arr[], int num,int low, int high,int x ){
+
+    while(low <= high){
+        int mid = (low+high)/2;
+        if(arr[mid] == x ){
+            return mid;
+        }
+        if(arr[mid] < x){
+            return binSrchRec(arr, num, mid+1, high, x);
+        }
+        if(arr[mid] > x){
+            return binSrchRec(arr, num, low, mid-1, x);
+        }
+    }
+    return -1;
+    
+}
 int main(){
     int num;
     cin>> num;
@@ -33,6 +51,7 @@ int main(){
     for(int i= 0; i< num; i++){
         cin >> arr[i];
     }
-    cout <<binSrchIter(arr, num, x);
+    // cout <<binSrchIter(arr, num, x);
+    cout <<binSrchRec(arr, num, 0, num-1, x );
     return 0;
 }
