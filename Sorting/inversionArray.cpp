ce8b6ffa645f6452e57240abae5d5dd2438cdc4d
gulshanyadav01 <gulshany01@gmail.com>
2020-10-16 17:49:33 +0530

    yet to come

--- Sorting/inversionArray.cpp
+++ Sorting/inversionArray.cpp
@@ -5,6 +5,7 @@ date: 16-oct-2020
 #include<iostream>
 #include<algorithm>
 using namespace std;
+// this take Q(n^2 ) time 
 int  inversionArray(int arr[], int num){
     int count= 0;
     for(int i= 0; i< num-1; i++){
