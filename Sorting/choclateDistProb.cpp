1db375ec5e8dc87cbccfecb7222d8355c7b4620b
gulshanyadav01 <gulshany01@gmail.com>
2020-10-19 12:35:26 +0530

    yet to come

---
+++ Sorting/choclateDistProb.cpp
@@ -0,0 +1,27 @@
+/* 
+author: *gulshan yadav*
+date: 19-oct-2020
+*/
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int choclateDistProb(int arr[], int num, int child){
+    sort(arr, arr+num);
+    int res= arr[child-1]- arr[0];
+    for(int i= 1; i+child-1< num; i++){
+        res = min(arr[i + child - 1]-arr[i], res);
+    }
+    return res;
+
+}
+int main(){
+    int num,child; 
+    cin >> num >> child;
+    int arr[num];
+    for(int i= 0; i< num; i++){
+        cin >> arr[i];
+
+    }
+    cout << choclateDistProb(arr, num , child);
+    return 0;
+}
