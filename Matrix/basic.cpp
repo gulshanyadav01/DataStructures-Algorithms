907e255d7fee8350a26c15e44a1c6b7d6dc086d1
gulshanyadav01 <gulshany01@gmail.com>
2020-10-19 17:02:45 +0530

    yet to come

---
+++ Matrix/basic.cpp
@@ -0,0 +1,24 @@
+/* 
+author: *gulshan yadav*
+date: 19-oct-2020
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row, col;
+    cin >> row >> col;
+    int arr[row][col];
+    for(int i= 0; i< row; i++){
+        for(int j= 0; j< col; j++){
+            cin >> arr[i][j];
+        }
+    }
+    for(int i = 0; i< row; i++){
+        for(int j= 0; j< col; j++){
+            cout << arr[i][j]<< " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
