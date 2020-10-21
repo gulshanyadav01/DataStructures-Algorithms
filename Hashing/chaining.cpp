b84067c1727fb6aed4afc769163059ab2adc54cf
gulshanyadav01 <gulshany01@gmail.com>
2020-10-21 16:51:11 +0530

    yet to come

--- Hashing/chaining.cpp
+++ Hashing/chaining.cpp
@@ -15,11 +15,34 @@ class myHash{
     myHash(int b){
         bucket = b;
         table = new list<int> [b];
-        
+
+    }
+    void insert(int key ){
+        int i = key% bucket;
+        table[i].push_back(key);
     }
 
 
+    bool search(int key) {
+        int i = key % bucket; 
+        for(auto x: table[i]){
+            if(x == key ){
+                return true;
+            }
+        }
+        return false;
+    }
 
 };
 
+int main(){
+    cout << "enter the no. of key "<< endl;
+    int key; 
+    cin >> key; 
+
+    myHash hey(key);
+    hey.insert(10);
+    cout << hey.search(1000);
+    return 0;
+}
 
