145365030bf5c5fa45261dce5e0d60c16e875a3e
gulshanyadav01 <gulshany01@gmail.com>
2020-10-22 17:37:22 +0530

    yet to come

---
+++ Hashing/unionOfTwoArr.cpp
@@ -0,0 +1,56 @@
+/* 
+author: *gulshan yadav*
+date: 22-oct-2020
+*/
+#include<iostream>
+#include<algorithm>
+#include<list>
+#include<vector>
+#include<unordered_set>
+#include<bits/stdc++.h>
+using namespace std;
+
+int unionOfTwoArray(int arr1[], int arr2[], int num1, int num2){
+
+    int temp[num1 + num2]; 
+    int index= 0; 
+    int res = 0;
+    for(int i =0; i< num1; i++){
+        temp[index] = arr1[i];
+        index++;
+    }
+    for(int j= 0; j< num2; j++){
+        temp[index] = arr2[j];
+        index++;
+    }
+    for(int i=0 ; i< index; i++){
+        bool flag = false;
+        for(int j= i-1; j>= 0; j--){
+            if(temp[i] == temp[j]){
+                flag= true;
+                break;
+            }
+        }
+        if(flag == false){
+            res++;
+        }
+        
+    }
+    return res;
+
+}
+
+int main(){
+    int num1, num2; 
+    cin >> num1 >> num2 ;
+    int arr1[num1]; 
+    int arr2[num2];
+    for(int i = 0; i< num1; i++){
+        cin >> arr1[i]; 
+    }
+    for(int i = 0; i< num2; i++){
+        cin >> arr2[i]; 
+    }
+    cout << unionOfTwoArray(arr1, arr2, num1, num2);
+    return 0;
+}
