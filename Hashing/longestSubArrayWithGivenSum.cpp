047d56a48efc77b1f5367c685eb8e88aa5676725
gulshanyadav01 <gulshany01@gmail.com>
2020-10-23 16:11:00 +0530

    yet to come

--- Hashing/longestSubArrayWithGivenSum.cpp
+++ Hashing/longestSubArrayWithGivenSum.cpp
@@ -7,8 +7,13 @@ date: 23-oct-2020
 #include<list>
 #include<vector>
 #include<unordered_set>
+#include<unordered_map>
 #include<bits/stdc++.h>
 using namespace std;
+
+
+// this is naive approach 
+// time complexity is O(n^2);
 
 int longestSubArrayWithGivenSum(int arr[], int num, int k){
     int count = 0; 
@@ -24,6 +29,26 @@ int longestSubArrayWithGivenSum(int arr[], int num, int k){
     return count; 
 }
 
+// efficient Algorithm
+
+int longestSubArray(int arr[], int num, int sum ){
+    int res = 0; 
+    int preSum = 0; 
+    unordered_map<int, int> m; 
+    for(int i =0; i< num; i++){
+        preSum += arr[i];
+        if(preSum == sum) res = i+1;
+        if(m.find(preSum) == m.end()){
+            m.insert({preSum, i});
+        }
+        if(m.find(preSum-sum) != m.end()){
+            res = max(res, i-m[preSum-sum]);
+        }
+        
+    }
+    return res;
+}
+
 int main(){
     int num; 
     cin >> num; 
@@ -34,6 +59,7 @@ int main(){
 
     int k; 
     cin >> k;
-    cout << longestSubArrayWithGivenSum(arr, num , k);
+    // cout << longestSubArrayWithGivenSum(arr, num , k);
+    cout << longestSubArray(arr, num , k);
     return 0;
 }
