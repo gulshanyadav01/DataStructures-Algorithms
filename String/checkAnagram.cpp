eb1a464e8e690d833125474c3b5ac536c29fabb9
gulshanyadav01 <gulshany01@gmail.com>
2020-10-24 16:51:25 +0530

    yet to come

--- String/checkAnagram.cpp
+++ String/checkAnagram.cpp
@@ -7,6 +7,7 @@ Date: 24-oct-2020
 using namespace std;
 
 // naive solution 
+// time complexity of this solution is O(nlogn);
 
 bool checkAnagram(string str1, string str2){
     sort(str1.begin(), str1.end());
@@ -14,10 +15,30 @@ bool checkAnagram(string str1, string str2){
     return str1 == str2;
 }
 
+
+// efficient solution 
+
+bool checkAnagram2(string str1, string str2){
+    int count[26] = {0};
+    if(str1.length() != str2.length()) return false;
+
+    for(int i =0; i< str1.length(); i++){
+        count[str1[i]-'a']++;
+        count[str2[i]-'a']--;
+    }
+    for(int i= 0; i< 26; i++){
+        if(count[i] != 0){
+            return false;
+        }
+    }
+    return true;
+}
+
 int main(){
     string str1, str2; 
     cin >> str1; 
     cin >> str2; 
-    cout << checkAnagram(str1, str2);
+    // cout << checkAnagram(str1, str2);
+    cout << checkAnagram2(str1, str2);
     return 0;
 }
