f3d299db000c751b812478240243492a7961c6b9
gulshanyadav01 <gulshany01@gmail.com>
2020-10-24 17:51:40 +0530

    yet to come

--- String/leftMostRepeat.cpp
+++ String/leftMostRepeat.cpp
@@ -8,6 +8,7 @@ using namespace std;
 const int CHAR = 256;
 
 // naive solution 
+// time complexity O(n^2)
 int leftMostRepeat(string s){
     int res = 0;
     for(int i = 0; i< s.length()-1; i++){
@@ -17,9 +18,46 @@ int leftMostRepeat(string s){
     }
     return -1; 
 }
+
+// efficient solution 
+
+int leftMostRepeat2(string s){
+    int count[CHAR] = {0};
+    for(int i = 0; i< s.length(); i++){
+        count[s[i]]++;
+    }
+    for(int i = 0; i< s.length(); i++){
+        if(count[s[i]] > 1){
+            return i;
+        }
+    }
+    return -1;
+}
+
+// second most efficinet solution 
+
+int leftMostRepeat3(string s){
+    int res = INT_MAX;
+    int fIndex[CHAR]; 
+    fill(fIndex, fIndex+ CHAR, -1); 
+    for(int i =0; i< s.length(); i++){
+        int f = fIndex[s[i]]; 
+
+        if(f == -1 ){
+            fIndex[s[i]] = i; 
+        }
+        else {
+            res = min(res, f);
+        }
+    }
+    return ( res == INT_MAX) ? -1: res; 
+}
+
+
 int main(){
     string s; 
     cin >> s; 
-    cout << leftMostRepeat(s);
+    cout << leftMostRepeat3(s);
+
     return 0;
 }
