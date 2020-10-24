723df87c2d652610207e0f4935d32e5177d22743
gulshanyadav01 <gulshany01@gmail.com>
2020-10-24 16:56:20 +0530

    yet to come

--- String/checkAnagram.cpp
+++ String/checkAnagram.cpp
@@ -5,7 +5,7 @@ Date: 24-oct-2020
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-
+const int CHAR = 256;
 // naive solution 
 // time complexity of this solution is O(nlogn);
 
@@ -19,14 +19,14 @@ bool checkAnagram(string str1, string str2){
 // efficient solution 
 
 bool checkAnagram2(string str1, string str2){
-    int count[26] = {0};
+    int count[CHAR] = {0};
     if(str1.length() != str2.length()) return false;
 
     for(int i =0; i< str1.length(); i++){
-        count[str1[i]-'a']++;
-        count[str2[i]-'a']--;
+        count[str1[i]]++;
+        count[str2[i]]--;
     }
-    for(int i= 0; i< 26; i++){
+    for(int i= 0; i< CHAR; i++){
         if(count[i] != 0){
             return false;
         }
