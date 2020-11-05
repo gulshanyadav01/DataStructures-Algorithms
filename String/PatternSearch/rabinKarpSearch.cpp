a817655c4ee0894c4ebab9f0e8db5e9d35b64f04
gulshanyadav01 <gulshany01@gmail.com>
2020-11-05 16:42:03 +0530

    pattern search

--- String/PatternSearch/rabinKarpSearch.cpp
+++ String/PatternSearch/rabinKarpSearch.cpp
@@ -1,7 +1,55 @@
 /*
 Author: "gulshan yadav"
-Date: 28-oct-2020
+Date: 05-Nov-2020
 */
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+#define d 256
+
+void rabinKarpSearch(string text, string pat,int q){ 
+    int m = pat.length(); 
+    int n = text.length(); 
+    
+    int h = 1; 
+    for(int i = 1; i<= m-1; i++){
+        h = (h*d) % q; 
+    }
+
+    int p= 0, t= 0; 
+    for(int i = 0; i< m; i++){
+        p = (p*d + pat[i]) % q ; 
+        t = (t*d + text[i]) % q; 
+    }
+
+    for(int i = 0; i<= n-m; i++){
+
+        if(p == t){
+            bool flag = true; 
+            for(int j= 0; j< m; j++){
+                if(text[i+j] != pat[j]) flag = false; break;
+                
+            }
+            if(flag) cout << i<< " "; 
+            if(i< n-m){ 
+                t = (d*(t - (text[i] * h)) + text[i+m]) %q; 
+            }
+
+        }
+    }
+
+}
+
+int main(){
+    cout << "enter the text string"<<"\n";
+    string text; 
+    cin >> text; 
+    cout << "enter the pattern string"<< "\n";
+    string pat; 
+    cin >> pat;
+    // naiveSearch(text, pat);
+    rabinKarpSearch(text, pat, 101);
+
+    return 0;
+    
+}
