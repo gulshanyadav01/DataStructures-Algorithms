16cf64fcee6bf0d7cb09dccd78cc743c8d7240ae
gulshanyadav01 <gulshany01@gmail.com>
2020-11-07 15:54:26 +0530

    revision

---
+++ Array/Revision/secondLar.cpp
@@ -0,0 +1,37 @@
+/*
+
+author:"gulshan yadav",
+date: 07-NOV-2020
+
+*/
+
+#include<iostream>
+using namespace std; 
+
+int secondLargest(int arr[], int num){
+    int res = -1; 
+    int lar = arr[0]; 
+
+    for(int i = 1; i< num; i++){
+        if(arr[i] > lar){
+            res = lar;
+            lar = arr[i]; 
+        }
+
+       else  if(arr[i]> res || res == -1){
+            res = arr[i]; 
+        }
+    }
+    return res; 
+}
+
+int main(){
+    int num; 
+    cin >> num; 
+    int arr[num]; 
+    for(int i = 0; i< num; i++){
+        cin >> arr[i];
+    }
+    cout << secondLargest(arr, num); 
+    return 0;
+}
