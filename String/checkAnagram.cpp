ee32d107852954d344afb2739a8ecd905bb7536a
gulshanyadav01 <gulshany01@gmail.com>
2020-11-16 21:13:57 +0530

    yet to come

--- String/checkAnagram.cpp
+++ String/checkAnagram.cpp
@@ -12,7 +12,7 @@ const int CHAR = 256;
 bool checkAnagram(string str1, string str2){
     sort(str1.begin(), str1.end());
     sort(str2.begin(), str2.end());
-    return str1 == str2;
+    return str1 == str; 
 }
 
 
