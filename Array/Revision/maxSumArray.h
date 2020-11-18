9387efd7455317f7605e1d32a9b2a36da64a0bdd
gulshanyadav01 <gulshany01@gmail.com>
2020-11-18 16:47:13 +0530

    max sum of sub array

--- Array/Revision/maxSumArray.h
+++ Array/Revision/maxSumArray.h
@@ -4,11 +4,11 @@ date : 18-NOV-2020
 */
 
 int maxSumArray(int arr[], int num){
-    int res = 0; 
+    int res  = arr[0]; 
     for(int i = 0; i< num; i++){
         int sum = arr[i]; 
         for(int j = i+1; j< num; j++){
-            
+
             sum += arr[j]; 
         }
         res = max(res, sum); 
