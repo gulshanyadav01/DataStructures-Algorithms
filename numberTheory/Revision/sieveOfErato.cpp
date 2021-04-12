85b5b9cac75df631cb87b78c7c395d9e1db481df
gulshanyadav01 <gulshany01@gmail.com>
2021-04-12 13:22:36 +0530

    sieve is done

---
+++ numberTheory/Revision/sieveOfErato.cpp
@@ -0,0 +1,30 @@
+/* 
+author: gulshan yadav
+date: 12-April-2021
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void sieve(int num){
+    vector<bool> isPrime(num+1, true); 
+    for(int i = 2; i*i <= num; i++){
+        if(isPrime[i]){
+            for(int j = 2*i; j<= num; j = j+i){
+                isPrime[j] = false;
+            }
+        }
+    }
+    for(int i = 2; i<= num; i++){
+        if(isPrime[i]){
+            cout << i << " "; 
+        }
+    }
+}
+int main(){
+    int num; 
+    cin >> num; 
+    sieve(num); 
+    return 0; 
+}
