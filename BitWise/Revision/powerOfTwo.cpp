3fb529d208f3c9988974b3d28ffba48cc84f5734
gulshanyadav01 <gulshany01@gmail.com>
2021-04-16 16:50:16 +0530

    power of two is done

--- BitWise/Revision/powerOfTwo.cpp
+++ BitWise/Revision/powerOfTwo.cpp
@@ -6,17 +6,25 @@ date: 16-April-2021
 using namespace std;
 
 bool  powerOfTwo(int num){
-    while(num != 1){
+    while(num != 1) {
         if(num % 2 != 0 ) return false; 
         num /= 2; 
     }
     return true; 
 }
 
+// efficient method  
+
+bool isPowerTwo(int num){
+    if (num ==0 ) return false; 
+    return ((num & (num-1)) == 0); 
+}
+
 int main(){
     int num; 
     cin >> num; 
-    cout << powerOfTwo(num); 
+    // cout << powerOfTwo(num);
+    cout << isPowerTwo(num); 
     return 0; 
 
 }
