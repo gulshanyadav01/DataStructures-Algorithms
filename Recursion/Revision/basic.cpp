adb07db38bf9f4424a47e3666ff5e10156d03a30
gulshanyadav01 <gulshany01@gmail.com>
2021-04-26 12:57:21 +0530

    recurrence relation is done

---
+++ Recursion/Revision/basic.cpp
@@ -0,0 +1,29 @@
+/* 
+author: gulshan yadav
+date: 26-April-2021
+*/
+#include<iostream>
+using namespace std;
+
+void fun(int n){
+    if(n == 0 ){
+        return ; 
+    }
+    fun(n-1); 
+    cout << n << " "; 
+    fun(n-1); 
+}
+int  fun2(int n){
+    if(n == 1){
+        return 0; 
+    }
+    return 1+ fun2(n/2); 
+
+}
+int main(){
+    int n; 
+    cin >> n; 
+    // fun(n); 
+    cout << fun2(n); 
+    return 0; 
+}
