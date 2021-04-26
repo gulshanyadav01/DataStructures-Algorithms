a76a5b4d5644bf9d56840b15fc4a2b1b1184a9a3
gulshanyadav01 <gulshany01@gmail.com>
2021-04-26 17:49:12 +0530

    tower of hanoi is done

---
+++ Recursion/Revision/towerOfHanoi.cpp
@@ -0,0 +1,24 @@
+/* 
+author: gulshan yadav
+date: 26-April-2021
+*/
+#include<iostream>
+using namespace std;
+
+void Toh(int num, char a , char b, char c){
+    if( num == 1){
+        cout << "move disk "<<  num << " from " << a << " to " << c << endl;
+        return; 
+    }
+    Toh(num-1, a, c,  b); 
+    cout << "move disk "<< num <<" from "  << a <<" to " << c<< endl;
+    Toh(num-1, b, a, c); 
+}
+
+int main(){
+    int num; 
+    cin >> num; 
+    char a, b, c; 
+    cin >> a >> b >>c; 
+    Toh(3, a, b, c );
+}
