a6a3770f0e6ad64580d3bac3d3162ec5e8c52c51
gulshanyadav01 <gulshany01@gmail.com>
2021-05-10 12:02:37 +0530

    max sum sub array

---
+++ Array/Revision2/maxSumSubArray.cpp
@@ -0,0 +1,45 @@
+/* 
+author: gulshan yadav
+date: 10-May-2021
+*/
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int maxSumSubArray(int arr[], int num){
+    int res = arr[0]; 
+    for(int i = 0; i< num; i++){
+        int curr = 0; 
+        for(int j = i; j< num; j++){
+            curr += arr[j]; 
+            res = max(curr, res);
+        }
+    }
+    return res; 
+}
+
+// efficient solution 
+
+int maxSum(int arr[], int num){
+    int sum = arr[0]; 
+    int res =  arr[0]; 
+    for(int i = 0;i< num; i++){
+        sum  = max(arr[i], arr[i]+ sum); 
+        res  = max(res, sum);
+
+    }
+    return sum; 
+}
+
+int main(){
+    int num; 
+    cin >> num; 
+    int arr[num]; 
+    for(int i = 0; i< num; i++){
+        cin >> arr[i]; 
+    }
+    cout << maxSumSubArray(arr, num); 
+    // cout << maxSum(arr, num); 
+    return 0; 
+}
