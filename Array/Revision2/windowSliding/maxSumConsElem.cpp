84556c18ee887fb2d39d82cbaa1c97fb26da5481
gulshanyadav01 <gulshany01@gmail.com>
2021-05-12 11:46:29 +0530

    max sum cons element is done

---
+++ Array/Revision2/windowSliding/maxSumConsElem.cpp
@@ -0,0 +1,49 @@
+/* 
+author: gulshan yadav
+date: 12-May-2021
+*/
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxSumKConsElem(int arr[], int num, int k ){
+    int res = INT_MIN;
+    for(int i =0; i<= num-k; i++){
+        int sum = 0; 
+        for(int j = 0; j< k; j++){
+            sum += arr[i+j];
+        }
+        res = max(res, sum);
+    }
+    return res; 
+}
+
+// efficient solution :
+// precompute the window of k 
+// then add one to it and subtract one to it
+int maxSum(int arr[], int num, int k){
+    int windowSum = 0; 
+    int res =  INT_MIN; 
+    for(int i= 0; i< k; i++ ){
+        windowSum += arr[i];
+    }
+    for(int i = 0; i< num-k; i++){
+        windowSum = windowSum-arr[i] + arr[k+i];
+        res = max(res, windowSum);
+    }
+    return res; 
+}
+
+int main(){
+    int num; 
+    cin >> num; 
+    int arr[num]; 
+    for(int i =0; i< num; i++){
+        cin >> arr[i];
+    }
+    int k; 
+    cin >> k;
+    // cout << maxSumKConsElem(arr, num, k);
+    cout << maxSum(arr, num, k); 
+    return 0; 
+}
