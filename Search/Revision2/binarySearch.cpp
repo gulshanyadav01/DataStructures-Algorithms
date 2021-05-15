7aa97e6a0496b6b28a7915a3512246386f8ad1f2
gulshanyadav01 <gulshany01@gmail.com>
2021-05-15 15:38:31 +0530

    binary search is done

---
+++ Search/Revision2/binarySearch.cpp
@@ -0,0 +1,33 @@
+/* 
+author: gulshan yadav
+date: 15-May-2021
+*/
+#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+int binarySearch(int arr[], int num, int key){
+    int low = 0; 
+    int high = num-1; 
+    while(low<= high){
+        int mid = (low+high)/2;
+        if(arr[mid] == key) return mid; 
+        else if(arr[mid] < key) low = mid+1; 
+        else if(arr[mid] > key) high = mid-1;
+    }
+    return -1; 
+}
+
+int main(){
+    int num; 
+    cin >> num; 
+    int arr[num];
+    for(int i = 0; i< num; i++){
+        cin >> arr[i];
+    }
+    int key;
+    cin >> key; 
+    cout << binarySearch(arr, num, key);
+    return 0; 
+}
