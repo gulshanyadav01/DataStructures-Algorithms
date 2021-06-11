4c1a48d48493ce67854edabd88dd5206cbbba567
gulshanyadav01 <gulshany01@gmail.com>
2021-06-11 22:29:40 +0530

    doubly linked list is done

---
+++ LinkedList/doublyLinkedList.cpp
@@ -0,0 +1,24 @@
+/* 
+author: gulshan yadav
+date: 10-june-2021
+email: gulshany01@gmail.com
+*/
+#include<iostream>
+#include "DNode.h"
+#include "print.h"
+using namespace std; 
+
+
+
+int main(){
+    Node *head = new Node(10); 
+    Node *temp1 = new Node(20);
+    Node *temp2 = new Node(30);
+    head->next = temp1; 
+    temp1->prev = head; 
+    temp1 -> next = temp2; 
+    temp2-> prev = temp1; 
+    print(head); 
+    return 0; 
+
+}
