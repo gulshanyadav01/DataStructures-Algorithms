81fa2b5844d5117f55cc167feabdb9e8b5fd3359
gulshanyadav01 <gulshany01@gmail.com>
2021-06-22 12:11:56 +0530

    balanced parenthesis is not done yet

---
+++ Stack/basic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stack>
+using namespace std; 
+int main(){
+    stack<int> s; 
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    while(s.empty() != true){
+        cout << s.top()<<" ";
+        s.pop();
+    }
+    return 0; 
+}
