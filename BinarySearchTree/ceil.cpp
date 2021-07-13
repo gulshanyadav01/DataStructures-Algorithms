1e44241fe6de65c0b1b2ad58fc39acae9cb707c2
gulshanyadav01 <gulshany01@gmail.com>
2021-07-13 15:49:55 +0530

    floor and ceil has been done

---
+++ BinarySearchTree/ceil.cpp
@@ -0,0 +1,40 @@
+/*
+author:gulshan yadav
+email: gulshany01@gmail.com
+date: 13-July-2021
+*/
+#include<iostream>
+#include<bits/stdc++.h>
+#include "/home/gulshan/Desktop/geekForGeeks/Tree/Node.h"
+using namespace std;
+
+// efficient solution 
+
+Node *ceil(Node *root, int x){
+    Node *res = NULL; 
+    while(root!= NULL){
+        if(root-> data == x) return root; 
+        else if (root-> data > x){
+            res = root; 
+            root = root-> left;
+        }
+        else if(root-> data < x){
+            root = root-> right;
+        }
+    }
+    return res; 
+}
+
+int main(){
+    Node *root = new Node(15); 
+    root-> left = new Node(5); 
+    root-> right = new Node(20);
+    root-> left-> left = new Node(3); 
+    root-> right -> right = new Node(80);
+    root-> right -> left = new Node(18);
+    root-> right -> left -> left = new Node(16);
+    Node *k = ceil(root, 21);
+    cout << k-> data;
+    return 0; 
+}
+
