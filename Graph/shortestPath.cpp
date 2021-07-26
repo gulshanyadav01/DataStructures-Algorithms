bb988f008b073196ac9a6cdc69940c0c7fce2699
gulshanyadav01 <gulshany01@gmail.com>
2021-07-26 20:46:16 +0530

    shortest path has been done

--- Graph/shortestPath.cpp
+++ Graph/shortestPath.cpp
@@ -9,20 +9,27 @@ using namespace std;
 
 
 void shortestPath(vector<int> adj[], int v, int s){
+    // create a distance array with infinite value; 
     int dist[v]; 
     bool visited[v]; 
     for(int i =0 ; i< v; i++){
         dist[i] = INT_MIN; 
         visited[i] = false; 
     }
+    
+    // create a queue
     queue<int> q; 
     q.push(s); 
+    // insert source vertex 
     dist[s]  = 0; 
     visited[s] = true; 
+    // travese the queue
     while(q.empty() == false){
         int u = q.front(); 
         q.pop(); 
+        // travese every vertex of u
         for(int i: adj[u]){
+            // if not visited then visit
             if(visited[i] == false){
                 dist[i] = dist[u] + 1;
                 visited[i] = true; 
@@ -55,5 +62,6 @@ int main(){
     addEdge(adj, 2, 3); 
     shortestPath(adj, v, 0); 
     return 0; 
-    
+
+
 }
