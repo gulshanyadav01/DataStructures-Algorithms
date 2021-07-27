4de90fc1cb8868d3788e219b7b4a7a4dcd08139f
gulshanyadav01 <gulshany01@gmail.com>
2021-07-27 11:19:40 +0530

    lru cache has been done

--- LinkedList/cloneLinkedListRandom.cpp
+++ LinkedList/cloneLinkedListRandom.cpp
@@ -30,7 +30,7 @@ Node *cloneLinkedList(Node *head){
         hmap[curr] = new Node(curr-> data);
     
     for(Node *curr = head; curr != NULL; curr = curr -> next){
-            Node *cloneCurr = hmap[curr]; 
+            Node *cloneCurr = hmap[curr];
             cloneCurr -> next = hmap[curr -> next]; 
             cloneCurr -> random = hmap[curr -> random];
     }
@@ -38,6 +38,42 @@ Node *cloneLinkedList(Node *head){
     Node *head2 = hmap[head]; 
     return head2; 
 }
+
+
+// method 2: 
+// O(N)
+// space : O(1)
+
+Node *clone(Node *head){
+
+    for(Node *curr = head; curr != NULL;){
+        Node *next = curr -> next; 
+        curr -> next  = new Node(curr -> data); 
+        curr -> next -> next = next; 
+        curr = next; 
+    }
+
+    for(Node *curr = head; curr != NULL; curr = curr-> next->next){
+        curr->next->random = (curr-> random != NULL) ? curr -> random -> next : NULL; 
+    }
+
+    Node* original = head, *copy = head->next; 
+  
+    Node *temp = copy; 
+  
+    while (original && copy) 
+    { 
+        original->next = 
+         original->next? original->next->next : original->next; 
+  
+        copy->next = copy->next?copy->next->next:copy->next; 
+        original = original->next; 
+        copy = copy->next; 
+    } 
+  
+    return temp;
+}
+
 
 // print the node 
 
@@ -60,7 +96,7 @@ int main(){
     head -> next -> random  = head -> next -> next -> next; 
     head -> next -> next -> next -> random = head -> next -> next; 
     head -> next -> next -> next -> next -> random = head -> next -> next -> next; 
-    Node *head2 = cloneLinkedList(head); 
+    Node *head2 = clone(head); 
     print(head2);
     return 0;  
 }
