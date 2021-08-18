c64df20e88a2c6c559026c95e37aefc48f85f772
gulshanyadav01 <gulshany01@gmail.com>
2021-08-18 17:21:17 +0530

    subset II has been done

--- Backtracking/subsetSum.cpp
+++ Backtracking/subsetSum.cpp
@@ -1,6 +1,11 @@
+/*
+author:gulshan yadav
+email: gulshany01@gmail.com
+date: 18-Aug-2021
+*/
 #include<iostream>
 #include<bits/stdc++.h>
-using namespace std; 
+using namespace std;
 
 void subsetSum(int index, vector<int> &arr, int sum, vector<int> &ans){
     if(index == arr.size()){
