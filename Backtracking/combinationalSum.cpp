d1615de730367da84c907dca369b82768d4ab691
gulshanyadav01 <gulshany01@gmail.com>
2021-08-19 21:14:35 +0530

    dynamic programming

--- Backtracking/combinationalSum.cpp
+++ Backtracking/combinationalSum.cpp
@@ -7,19 +7,19 @@ date: 17-Aug-2021
 #include<bits/stdc++.h>
 using namespace std;
 
-void combinationalSum(int ind, int target,  vector<vector<int>> &ans, vector<int> &arr, vector<int> &aux, int count, int rs){
+void combinationalSum(int ind, int target,  vector<vector<int>> &ans, vector<int> &arr, vector<int> &aux){
 
     if(ind == arr.size()){
         if(target == 0){
             ans.push_back(aux);
-            rs = min(rs, count);
+           
 
         }
         return; 
     }
-    int count = 0; 
+    
     if(arr[ind]<= target){
-
+        
         aux.push_back(arr[ind]); 
         combinationalSum(ind, target-arr[ind], ans, arr, aux); 
         aux.pop_back();
@@ -50,7 +50,9 @@ int main(){
     int target; 
     cin >> target; 
 
-    combinationalSum(0, target, ans, arr, aux, 0, INT_MAX);
+   
+
+    combinationalSum(0, target, ans, arr, aux);
 
     for(int i = 0; i< ans.size(); i++){
         for(int j = 0; j< ans[i].size(); j++){
@@ -58,6 +60,8 @@ int main(){
         }
         cout << endl;
     }
+
+
 
 
 }
