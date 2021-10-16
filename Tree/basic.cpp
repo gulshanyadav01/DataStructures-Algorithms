bad4ff4dc76d00907b09e3ae1a4e19f5f15ff35e
gulshanyadav01 <gulshany01@gmail.com>
2021-10-16 23:02:20 +0530

    basic has been changed

--- Tree/basic.cpp
+++ Tree/basic.cpp
@@ -32,11 +32,3 @@ int main(){
     cout << k <<endl;
     return 0; 
 }
-
-// int main(){
-//     Node *root = new Node(10); 
-//     root->left = new Node(20);
-//     root->right = new Node(30);
-//     root->left->left = new Node(40);
-//     return 0; 
-// }
